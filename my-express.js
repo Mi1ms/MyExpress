199a77abddb8cf780a625e2ebb0f7bcab4834f6b
Emilie G <emilie.granda@efrei.net>
2019-11-22 08:27:47 +0100

    method get / post

---
+++ my-express.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const myExpress = class Express {
+    
+    get(path, callback) {
+
+    }
+
+    post(path, callback) {
+
+    }
+}
+
+module.exports = {
+    myExpress,
+}
