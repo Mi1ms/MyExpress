79e90aa626b056491af4651107d750b1489a582e
ttcyvan <40865452+ttcyvan@users.noreply.github.com>
2019-12-08 18:00:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ app.listen(2222)
 app.get('', (req,res) =>{
 
 res.write('premier post' ,res);
-
+console.log('<h4>here in firt page </h4>')
+	res.write('<h1>firt page </h1>')
 })
+
+app.get('/data', function(req, res) {
+    console.log('<h4>here in call /hello</h4>')
+    // console.log("req idx: ", res);
+    res.write('<h1>page data</h1>')
+
+});
 
 app.post('/post', (req,res) =>{
-
-
+	
 })
+
+
+app.render('home', {name: 'Fona', text: 'i will crash you with my hammer !', age: 23.05976}, (err, html) => {
+  // console.log(err, html);
+});
+
+
